b681bf35adf5d966df791ac35a838556e5483f49
HARSHA4499 <shvrp4499@gmail.com>
2023-11-11 17:02:37 -0600

    Third Commit

--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,7 @@
 export const Task = (props) => {
     return (
-        <div className="task">
-          <h1 style={{color : props.completed ? 'green' :'red'}}>{props.taskName}</h1>
+        <div className="task" style={{backgroundColor : props.completed ? 'green' :'red'}}>
+          <h1>{props.taskName}</h1>
           <button onClick={()=>props.colorChanger(props.id)}>Complete</button>
           <button onClick={() => props.deleteTask(props.id)}>X</button>
         </div>
